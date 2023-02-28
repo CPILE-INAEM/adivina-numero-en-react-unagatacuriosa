a2f84363fbba09e0fb0da10f856df9edd675d644
unagatacuriosa <gabrielacanalesrubio@gmail.com>
2023-02-28 14:10:50 +0100

    Juego acabado

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ function App() {
     setNumber(Number(inputRef.current.value));
   };
 
+  const handleReset = () => {
+    setScore(20);
+    setNumber("");
+    setSecretNumber(randomNumber());
+    inputRef.current.value = "";
+  };
+
   useEffect(() => {
     console.log(`El numero secreto es ${secretNumber}`);
     console.log(`El numero introducido es ${number}`);
@@ -27,7 +34,11 @@ function App() {
       // mensaje ganado HECHO
       // mostar numero ganado HECHO
       // cambiar fondo
-      // si el escore es mayor actualizar
+
+      // si el escore es mayor actualizar HECHO
+      if (score > highscore) {
+        setHighscore(score);
+      }
     } else if (score === 1) {
       setScore(score - 1);
       // perder HECHO
@@ -41,13 +52,24 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [number]);
 
+  let statusBack;
+  if (secretNumber === number) {
+    statusBack = "win";
+  } else if (score === 0) {
+    statusBack = "lose";
+  } else {
+    statusBack = "playing";
+  }
+
   return (
-    <div>
+    <div className={statusBack}>
       {console.log("Renderizando APP")}
       <header>
         <h1>Guess My Number!</h1>
         <p className="between">(Between 1 and 20)</p>
-        <button className="btn again">Again!</button>
+        <button className="btn again" onClick={handleReset}>
+          Again!
+        </button>
         <div className="number">{number === secretNumber ? number : "?"}</div>
       </header>
       <main>
